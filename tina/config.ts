75d7176746322726f15838dd9bdde91a21c3b49c
Hampusfredrik <hampusfredrik@example.com>
2025-10-04 22:58:00 +0200

    Try TinaCMS v3 with simplified configuration

--- tina/config.ts
+++ tina/config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'tinacms'
 
-// Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINA_PUBLIC_CLIENT_ID || '07a73223-b78b-4839-b6df-857ebeeac361',
-  token: process.env.TINA_TOKEN || '6c1ad80e6155f541f657e748b336bcdb88dbc382',
+  clientId: '07a73223-b78b-4839-b6df-857ebeeac361',
+  token: '6c1ad80e6155f541f657e748b336bcdb88dbc382',
 
   build: {
     outputFolder: 'admin',
